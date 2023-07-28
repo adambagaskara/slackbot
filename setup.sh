610432683f46d9ebb8508a251a2a9ba13d3632db
Adam Bagaskara <adambagaskara01@yahoo.co.id>
2023-07-28 15:36:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ set -e
 
 # Install homebrew if not already installed
 if ! type brew >/dev/null 2>&1; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"\
+#  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"\
   </dev/null
 fi
 
